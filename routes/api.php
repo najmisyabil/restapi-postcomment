cdf25cfc5a210fbaa21a1ed5c62b29b9b11e3604
Syabil <najmisyabil@gmail.com>
2021-02-14 20:42:40 +0800

    changed post route to single get instead of resource because we only need 1 function

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('posts', PostController::class)->only(['index']);
+Route::get('posts', [PostController::class, 'index']);
